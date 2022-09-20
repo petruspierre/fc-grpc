aa06cd84f8e069e4a787ebb203b790d70df74ed6
Petrus Pierre <petrus.tsx@gmail.com>
2022-09-20 16:32:14 -0300

    register first service

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -4,7 +4,10 @@ import (
 	"log"
 	"net"
 
+	"github.com/petruspierre/go-grpc/pb"
+	"github.com/petruspierre/go-grpc/services"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
@@ -15,6 +18,8 @@ func main() {
 	}
 
 	grpcServer := grpc.NewServer()
+	pb.RegisterUserServiceServer(grpcServer, services.NewUserService())
+	reflection.Register(grpcServer)
 
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
